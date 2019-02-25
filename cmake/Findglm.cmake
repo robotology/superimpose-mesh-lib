ca9b7f500f17d516a08a4d05bee4698ecb4ea6e6
Claudio Fantacci <claudio.fantacci@gmail.com>
2019-02-25 15:18:37 +0100

    Clean up code in Findglm.cmake

--- cmake/Findglm.cmake
+++ cmake/Findglm.cmake
@@ -29,30 +29,33 @@
 # License text for the above reference.)
 # default search dirs
 
-SET(_glm_HEADER_SEARCH_DIRS
-"/usr/include"
-"/usr/local/include"
-"${CMAKE_SOURCE_DIR}/includes"
-"C:/Program Files (x86)/glm" )
-# check environment variable
-SET(_glm_ENV_ROOT_DIR "$ENV{GLM_ROOT_DIR}")
-IF(NOT GLM_ROOT_DIR AND _glm_ENV_ROOT_DIR)
-	SET(GLM_ROOT_DIR "${_glm_ENV_ROOT_DIR}")
-ENDIF(NOT GLM_ROOT_DIR AND _glm_ENV_ROOT_DIR)
-# put user specified location at beginning of search
-IF(GLM_ROOT_DIR)
-	SET(_glm_HEADER_SEARCH_DIRS "${GLM_ROOT_DIR}"
-	"${GLM_ROOT_DIR}/include"
-	${_glm_HEADER_SEARCH_DIRS})
-ENDIF(GLM_ROOT_DIR)
-# locate header
-FIND_PATH(GLM_INCLUDE_DIR "glm/glm.hpp"
-PATHS ${_glm_HEADER_SEARCH_DIRS})
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLM DEFAULT_MSG
-GLM_INCLUDE_DIR)
-IF(GLM_FOUND)
-	SET(GLM_INCLUDE_DIRS "${GLM_INCLUDE_DIR}")
-	MESSAGE(STATUS "GLM_INCLUDE_DIR = ${GLM_INCLUDE_DIR}")
-ENDIF(GLM_FOUND)
+set(_glm_HEADER_SEARCH_DIRS
+    "/usr/include"
+    "/usr/local/include"
+    "${CMAKE_SOURCE_DIR}/includes"
+    "C:/Program Files (x86)/glm")
 
+# Check environment variable
+set(_glm_ENV_ROOT_DIR "$ENV{GLM_ROOT_DIR}")
+
+if(NOT GLM_ROOT_DIR AND _glm_ENV_ROOT_DIR)
+    set(GLM_ROOT_DIR "${_glm_ENV_ROOT_DIR}")
+endif()
+
+# Put user specified location at beginning of search
+if(GLM_ROOT_DIR)
+	set(_glm_HEADER_SEARCH_DIRS "${GLM_ROOT_DIR}"
+	    "${GLM_ROOT_DIR}/include"
+	    ${_glm_HEADER_SEARCH_DIRS})
+endif()
+
+# Locate header
+find_path(GLM_INCLUDE_DIR "glm/glm.hpp"
+              PATHS ${_glm_HEADER_SEARCH_DIRS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GLM DEFAULT_MSG GLM_INCLUDE_DIR)
+
+if(GLM_FOUND)
+	set(GLM_INCLUDE_DIRS "${GLM_INCLUDE_DIR}")
+endif()
